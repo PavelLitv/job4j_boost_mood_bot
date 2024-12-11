9a9e92774bc7942a0f9658f292f15506010f1c6f
Pavel Litvinenko <korvine1980@yandex.ru>
2024-12-11 21:52:24 +0500

    Концепция AOP [#505309]

--- src/main/java/ru/job4j/aop/LoggingAspect.java
+++ src/main/java/ru/job4j/aop/LoggingAspect.java
@@ -1,4 +1,26 @@
 package ru.job4j.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+    @Pointcut("execution(* ru.job4j.bmb.services.*.*(..))")
+    private void serviceLayer() {
+    }
+
+    @Before("serviceLayer()")
+    public void logBefore(JoinPoint joinPoint) {
+        System.out.println(joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        var values = joinPoint.getArgs();
+        var params = ((MethodSignature) joinPoint.getSignature()).getParameterNames();
+        for (int i = 0; i < values.length; i++) {
+            System.out.println(params[i] + ": " + values[i]);
+        }
+    }
 }
